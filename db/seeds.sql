a7e16aa7e7fb8b428ceb6cf9601faf8f124113b5
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2020-05-08 10:31:30 -0700

    connection established and main handlebars put together

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO books (title,author)
+VALUES ("Les Fleurs Du Mal", "Charles Beaudelaire"),
+        ("Livro do Desassossego", "Fernando Pessoa"),
+        ("Leaves of Grass", "Walt Whitman")
